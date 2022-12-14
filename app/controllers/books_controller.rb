35205c394c40cec1340c9cff400c458adbb77a18
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2022-12-14 12:12:48 +0000

    [Add]editページ編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,13 +10,20 @@ class BooksController < ApplicationController
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to "/books/#{book.id}"
+    redirect_to book_path(book.id)
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    end
   
   private
   def book_params
